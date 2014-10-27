6de04fa40b7a1509e37c41514a8a37253bfb3145
cardorl <lemoscardoso86@gmail.com>
2014-10-26 22:30:41 -0200

    atualização sigma2_u_hat

--- sigma2_u_hat.R
+++ sigma2_u_hat.R
@@ -1,4 +1,4 @@
-sigma2.u.hat<-function(Sxx,Syy,Sxy,beta.hat,lambda.e){
+sigma2.u.hat.f<-function(Sxx,Syy,Sxy,beta.hat,lambda.e){
   sig.aux=(Syy-((2^beta.hat)*Sxy)+((beta.hat^2)*Sxx))/((beta.hat^2)*lambda.e);
   return(sig.aux);
   
